4e747a5c77bde2eb7b264a5c0728fbc7e8532cb4
plxgwalker <olegsuv.ukr@gmail.com>
2024-09-14 09:22:28 +0300

    feat: navbar icons

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,11 +1,35 @@
 import { Tabs } from 'expo-router';
+import Octicons from '@expo/vector-icons/Octicons';
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="search" options={{ title: 'Search' }} />
-      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
+    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather name="home" size={focused ? 30 : 25} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Octicons name="search" size={focused ? 30 : 25} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="settings-outline" size={focused ? 30 : 25} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
