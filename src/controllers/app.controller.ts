298bc42136823dec8aa7b7595db2673cfc6da9eb
Oleh <olegsuv.dev@gmail.com>
2024-10-20 16:50:39 +0300

    fix: vercel cron job with timeout for route

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { MIGRATION_REQ_TIMEOUT } from 'src/constants/api/utils';
+import { MigrationService } from 'src/services/jobs/migration.service';
+import { TimeoutInterceptor } from 'src/utils/timeout.interceptor';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(private readonly csvToSqlService: MigrationService) {}
 
   @Get()
-  getHello(): string {
+  @UseInterceptors(new TimeoutInterceptor(MIGRATION_REQ_TIMEOUT))
+  async getHello() {
+    // setted up mainly for vercel cron job
+    await this.csvToSqlService.runDailyMigration();
     return 'wassup from national cashback server';
   }
 }
---
+++ src/utils/timeout.interceptor.ts
@@ -0,0 +1,18 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, RequestTimeoutException } from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  constructor(private readonly timeout: number) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      timeout(this.timeout),
+      catchError((err) => {
+        if (err instanceof TimeoutError) return new RequestTimeoutException();
+        return err;
+      }),
+    );
+  }
+}
